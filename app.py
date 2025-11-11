e1eb6371399ac891bcb086f6bb42068bc60eaaf2
afnanulhaqque <afnanulhaq4@gmail.com>
2025-11-11 17:05:38 +0500

    First Commit

---
+++ app.py
@@ -0,0 +1,63 @@
+from flask import Flask, redirect , render_template , request, url_for
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+import os
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+db_path = os.path.join(basedir, "todo.db")
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{db_path}'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_ECHO'] = True
+db = SQLAlchemy(app)
+
+
+class Todo(db.Model):
+    Sr_No = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(200), nullable=False)  
+    desc = db.Column(db.String(500), nullable=False)
+    date_created = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def __repr__(self) -> str:
+        return f"{self.Sr_No} - {self.title}"
+
+@app.route('/', methods=['GET'])
+def index():
+    allTodo = Todo.query.all()
+    return render_template('index.html', allTodo=allTodo)
+
+@app.route('/submit', methods=['POST'])
+def submit():
+    title = request.form.get('todo-title', ' ').strip()
+    desc = request.form.get('todo-desc', ' ').strip()
+
+    if title and desc:
+        todo = Todo(title=title, desc=desc)
+        db.session.add(todo)
+        db.session.commit()
+        
+    return redirect(url_for('index'))
+
+@app.route('/delete/<int:Sr_No>')
+def delete(Sr_No):
+    # allTodo = Todo.query.filter_by(Sr_No=Sr_No).first()
+    # db.session.delete(allTodo)
+
+    Todo.query.filter_by(Sr_No=Sr_No).delete()
+
+    db.session.commit()
+    return redirect(url_for('index'))
+
+@app.route('/about')
+def about():
+    return render_template('about.html')    
+
+@app.route('/update')
+def update():
+    allTodo = Todo.query.all()
+    return str(allTodo)
+
+if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
+    app.run(debug=True , port=8000)
